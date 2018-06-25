108d4745bcd6c600a497febb79fa18377f509c78
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2018-06-25 20:13:41 +1200

    added an array of markers

--- client/components/Container.jsx
+++ client/components/Container.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Marker from './Marker';
-import {GoogleApiWrapper} from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react';
 import Map from './Map';
 
 class Container extends React.Component {
@@ -10,18 +10,100 @@ class Container extends React.Component {
     this.state = {
       markers: [
         {
+          id: 1,
           title: 'Westhaven',
           label: 'Westhaven',
           position: {
-            lat: -36.842277,
-            lng: 174.750224
+            lat: -36.839914,
+            lng: 174.747697
+          }
+        },
+        {
+          id: 2,
+          title: 'Okahu Bay Wharf',
+          label: 'Okahu Bay',
+          position: {
+            lat: -36.845246,
+            lng: 174.816951
+          }
+        },
+        {
+          id: 3,
+          title: 'Little Shoal Bay',
+          label: 'Little Shoal Bay',
+          position: {
+            lat: -36.816693,
+            lng: 174.739157
+          }
+        },
+        {
+          id: 4,
+          title: 'The Wharf',
+          label: 'The Wharf',
+          position: {
+            lat: -36.826638,
+            lng: 174.746667
+          }
+        },
+        {
+          id: 5,
+          title: 'Viaduct Events Centre',
+          label: 'Viaduct Events Centre',
+          position: {
+            lat: -36.841185,
+            lng: 174.759520
+          }
+        },
+        {
+          id: 6,
+          title: 'Cornwallis Wharf',
+          label: 'Cornwallis Wharf',
+          position: {
+            lat: -37.011840,
+            lng: 174.605286
+          }
+        },
+        {
+          id: 7,
+          title: 'Bucklands Beach',
+          label: 'Bucklands Beach',
+          position: {
+            lat: -36.862400,
+            lng: 174.899559
+          }
+        },
+        {
+          id: 8,
+          title: 'Magazine Bay',
+          label: 'Magazine Bay',
+          position: {
+            lat: -36.883566,
+            lng: 175.056192
+          }
+        },
+        {
+          id: 9,
+          title: 'Piha',
+          label: 'Piha',
+          position: {
+            lat: 36.954760,
+            lng: 174.468187
+          }
+        },
+        {
+          id: 10,
+          title: 'Hunua Falls',
+          label: 'Hunua Falls',
+          position: {
+            lat: 37.068781,
+            lng: 175.089827
           }
         }
       ]
     }
   }
 
-  render(){
+  render() {
     const style = {
       width: '100vh',
       height: '100vh'
@@ -30,7 +112,7 @@ class Container extends React.Component {
     return (
       <Map google={this.props.google} style={style}>
         {this.state.markers.map(marker => {
-          return <Marker title={marker.title} label={marker.label} position={marker.position} />
+          return <Marker key={marker.id} title={marker.title} label={marker.label} position={marker.position} />
         })}
       </Map>
     )
@@ -38,5 +120,5 @@ class Container extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'XXX'
+  apiKey: 'AIzaSyAPv7CZEnHS4ZJeuUA2x5Ls6YvX24D4WrI'
 })(Container)
