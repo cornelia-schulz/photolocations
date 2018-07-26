ba9ce2b5a2e326c00d36cd18a50a942574aa4263
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-27 07:54:34 +1200

    App test tests app component

---
+++ tests/client/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {configure, shallow} from 'enzyme'
+
+import App from '../../../client/components/App'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({adapter: new Adapter()})
+
+test('<App />', () => {
+  const expected = '<HashRouter />'
+  const wrapper = shallow(<App />)
+  expect(wrapper.text()).toBe(expected)
+})
