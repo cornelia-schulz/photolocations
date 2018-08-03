225c14e468be07fa3ab8fa525d0803cee5aaeb72
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-03 21:20:39 +1200

    db config

--- server/db/comments.js
+++ server/db/comments.js
@@ -1,6 +1,5 @@
 'use strict'
-const environment = process.env.NODE_ENV 
-// || 'development'
+const environment = process.env.NODE_ENV || 'production'
 const knex = require('knex')
 const config = require('./knexfile').development
 const connection = knex(config)
