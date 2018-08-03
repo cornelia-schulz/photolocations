5110b221b06c9466b63bb9bdbf0b5ecec354fa84
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-03 23:12:44 +1200

    change datatype

--- server/db/migrations/20180708172647_users.js
+++ server/db/migrations/20180708172647_users.js
@@ -5,9 +5,9 @@ exports.up = function(knex, Promise) {
     table.string('full_name')
     table.string('email')
     table.text('facebook_accessToken')
-    table.int('facebook_id')
+    table.integer('facebook_id')
     table.text('google_accessToken')
-    table.int('google_id')
+    table.integer('google_id')
   })
 }
 
