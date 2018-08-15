ca01c2168ea0d0296920ad77968da3a68c6e1c61
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-15 21:55:51 +1200

    Updated header CSS

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -12,26 +12,26 @@ class Header extends React.Component {
   render() {
     return (
 
-        <header>
-          <nav className="navbar" role="navigation">
-            <div className="logo">
-              <img src="/images/Logo.PNG" alt="Photo Locations" />
+      <header>
+        <nav className="navbar" role="navigation">
+          <div className="logo  header-left">
+            <img src="/images/Logo.PNG" alt="Photo Locations" />
+          </div>
+          <div className="header-center">
+            <Route exact path='/' component={Search} />
+          </div>
+          <div className="dropdown header-right">
+            <button className="dropbtn">
+              <i className="fa fa-bars" aria-hidden="true"></i>
+            </button>
+            <div className="dropdown-content">
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
             </div>
-            <div className="headerRight">
-              <Route exact path='/' component={Search} />
-              <div className="dropdown">
-                <button className="dropbtn">
-                  <i className="fa fa-bars" aria-hidden="true"></i>
-                </button>
-                <div className="dropdown-content">
-                  <Link to="/">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/contact">Contact</Link>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </header>
+          </div>
+        </nav>
+      </header>
 
     )
   }
