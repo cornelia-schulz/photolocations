1ec605c2c33d571d81b5397f6a23abdda7540e80
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-08-27 21:55:14 +1200

    for deployment

--- server/db/locations.js
+++ server/db/locations.js
@@ -15,7 +15,7 @@ function getAllLocations(testDb) {
   const db = testDb || connection
   return db('locations')
     .leftJoin('photos', 'locations.id', 'photos.location_id')	        
-    .leftJoin('ratings', 'locations.id', 'ratings.location_id')
+    // .leftJoin('ratings', 'locations.id', 'ratings.location_id')
     .select('locations.id as id', 'locations.title as title', 'locations.label as label', 'locations.lat as lat', 'locations.lng as lng', 'locations.info as description', 'photos.title as imageTitle', 'photos.url as url', 'locations.info_title as info')
 }
 
