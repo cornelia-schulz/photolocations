b17b78075dfd8395e781a5141ff04f627b9bb451
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-28 22:54:20 +1200

    added German strings for About and Contact pages

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
 import ReactGA from 'react-ga'
 import { I18n } from 'react-i18next'
+import i18n from 'i18next'
+import { connect } from 'react-redux'
 
 ReactGA.initialize('UA-124825499-1')
 ReactGA.pageview(window.location.pathname + window.location.search)
 
-const About = (props) => {
-  return (
-    <I18n ns="strings">
-      {
-        (t, { i18n }) => (
-          <div className="about">
-            <img src="/images/Whatipu.jpg" alt="Milkyway over Whatipu" />
-            <div className="aboutText">
-              <h1>{t('about.about_header')}</h1>
-              <p>{t('about.about_paragraph_1')}</p>
-              <p>{t('about.about_paragraph_2')}</p>
-              <p>{t('about.about_paragraph_3')}</p>
-              <p>Cornelia</p>
-            </div>
-          </div>
-        )
-      }
-    </I18n>
+class About extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      language: this.props.language
+    }
+  }
 
-  )
+  componentDidMount(){
+    i18n.changeLanguage(this.state.language)
+  }
+
+  render() {
+    return (
+      <I18n ns="strings" >
+        {
+          (t, { i18n }) => (
+            <div className="about">
+              <img src="/images/Whatipu.jpg" alt="Milkyway over Whatipu" />
+              <div className="aboutText">
+                <h1>{t('about.about_header')}</h1>
+                <p>{t('about.about_paragraph_1')}</p>
+                <p>{t('about.about_paragraph_2')}</p>
+                <p>{t('about.about_paragraph_3')}</p>
+                <p>Cornelia</p>
+              </div>
+            </div>
+          )
+        }
+      </I18n >
+    )
+  }
 }
 
-export default About
+function mapStateToProps(state) {
+  return {
+    language: state.receiveLanguage
+  }
+}
+
+export default connect (mapStateToProps, null)(About)
