e0e9f9e714b0c3db6fd1e75dec36fea29516913e
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-10-03 22:24:07 +1300

    adding instagram passport

--- server/passport.js
+++ server/passport.js
@@ -3,6 +3,7 @@ require('express')
 const passport = require('passport')
 const FacebookTokenStrategy = require('passport-facebook-token')
 const GoogleTokenStrategy = require('passport-google-token').Strategy
+const InstagramTokenStrategy = require('passport-instagram-token')
 //const config = require('./config.js')
 const user = require('./db/db')
 require('dotenv').config()
@@ -11,13 +12,15 @@ const facebookClientId = process.env.FACEBOOK_APP_ID
 const facebookSecret = process.env.FACEBOOK_SECRET
 const googleClientId = process.env.GOOGLE_CLIENT_ID
 const googleSecret = process.env.GOOGLE_SECRET
+const instagramClientId = process.env.INSTAGRAM_CLIENT_ID
+const instagramSecret = process.env.INSTAGRAM_SECRET
 
 module.exports = function () {
   passport.use(new FacebookTokenStrategy({
           clientID: facebookClientId,
           clientSecret: facebookSecret
       },
-      function (accessToken, refreshToken, profile, done) {
+      function(accessToken, refreshToken, profile, done) {
         console.log('facebook user')
         user.facebookUpsertUser(accessToken, refreshToken, profile, function(err, user) {
               return done(err, user)
@@ -29,10 +32,21 @@ module.exports = function () {
           clientSecret: googleSecret
       },
      // upsert === update if doesn't exist
-      function (accessToken, refreshToken, profile, done) {
+      function(accessToken, refreshToken, profile, done) {
           console.log('passport google user')
           user.googleUpsertUser(accessToken, refreshToken, profile, function(err, user) {
               return done(err, user)
           })
       }))
+
+      passport.use(new InstagramTokenStrategy({
+          clientID: instagramClientId,
+          clientSecret: instagramSecret,
+          passReqToCallback: true
+      },
+        function(req, accessToken, refreshToken, profile, next) {
+            User.findOrCreate({'instagram.id': profile.id}, function(error, user) {
+                return next(error, user)
+            })
+        }))
 }
